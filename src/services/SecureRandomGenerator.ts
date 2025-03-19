ad77f1d332642c57d9f0133a6dfaadbb66e03239
Masum Billah Zihadi <zihadimasumbillah@gmail.com>
2025-03-19 16:33:34 +0600

    secure random generator update

--- src/services/SecureRandomGenerator.ts
+++ src/services/SecureRandomGenerator.ts
@@ -3,7 +3,7 @@ import * as crypto from 'crypto';
 export class SecureRandomGenerator {
     static generateValue(max: number): { value: number; hmac: string; key: string } {
         const key = crypto.randomBytes(32).toString('hex').toUpperCase();
-        const value = crypto.randomInt(0, max - 1);
+        const value = crypto.randomInt(0, max);
         
         const hmac = crypto.createHmac('sha3-256', key)
             .update(value.toString())
